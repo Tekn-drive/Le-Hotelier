5851c1ba09fd97ff4882396f71f2647abb6489a1
Juan Arnold <89509753+Tekn-drive@users.noreply.github.com>
2023-06-03 23:45:28 +0700

    Delete src/application directory

--- src/application/Chef.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-public class Chef extends Employee{
-
-    public Chef(String id, String name){
-        super(id,name);
-    }
-
-}
--- src/application/Server.java
+++
@@ -1,8 +0,0 @@
-package application;
-
-public class Server extends Employee{
-
-    public Server(String id, String name){
-        super(id,name);
-    }
-}
